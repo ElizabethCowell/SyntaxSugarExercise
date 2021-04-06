90ad6831c8021752f156fc98d09d547fd9aabd43
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-06 12:08:15 -0400

    Solution

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;           
+            Console.WriteLine((answer < 9) ? $"{answer} is less than 9!" : $"{answer} greater than or equal to 9!");
+        }
+    }
+}
